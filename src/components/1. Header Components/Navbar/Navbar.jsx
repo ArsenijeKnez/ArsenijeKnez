6aa6b27d37a059bf7a5b8cd1a56a01914b24d8a5
Arsenije <arsenije.knezevic6@gmail.com>
2025-01-23 17:31:47 +0100

    import update

--- src/components/1. Header Components/Navbar/Navbar.jsx
+++ src/components/1. Header Components/Navbar/Navbar.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import './Navbar.css';
-
+import url from '/public/ArsenijeKnežević_CV.pdf';
 
 
 const Navbar = () => {
-  const url  = '/ArsenijeKnežević_CV.pdf';
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
